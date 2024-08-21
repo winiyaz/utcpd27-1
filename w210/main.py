98ff110bf4f7a5118eafe492a5e15a30c2a892be
winiyaz <winiyaz@proton.me>
2024-08-21 06:19:29 -0700

    😊 w210 - Add Widget Elements

---
+++ w210/main.py
@@ -0,0 +1,20 @@
+# Creating the various elements
+
+
+from tkinter import *
+
+# intiialize window
+window = Tk()
+window.title('Lesson w210')
+window.minsize(width=600, height=300)
+
+# --- Header Section
+header_label = Label(text='Lesson 210', font=('Arial', 24, "bold"))
+header_label.pack()
+
+#--- D27
+
+
+
+# -- default loop
+window.mainloop()
