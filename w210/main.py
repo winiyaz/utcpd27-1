7d69f82e3b4be60b07f7e600c136139ca1e21be2
winiyaz <winiyaz@proton.me>
2024-08-21 09:38:27 -0700

    😊 w210211 - Layouts for tkinter using grid

--- w210/main.py
+++ w210/main.py
@@ -7,6 +7,7 @@ from tkinter import *
 window = Tk()
 window.title('Lesson w210')
 window.minsize(width=600, height=300)
+window.config(padx=100, pady=100)
 
 # --- Header Section
 header_label = Label(text='Lesson 210', font=('Arial', 24, "bold"))
@@ -20,10 +21,10 @@ def bu1_click():
 	header_label.config(text=new_text)
 
 
-bu1 = Button(text="CLICKME", command=bu1_click, font=("Arial, 15"))
+bu1 = Button(text="CLICKME", command=bu1_click, font=("Courier New", 11))
 bu1.grid(column=2, row=2)
 
-bu2 = Button(text="Enter", font=("Arial, 15"))
+bu2 = Button(text="Enter", font=("Arial", 15))
 bu2.grid(column=4,row=4)
 
 # -- Text Entry
