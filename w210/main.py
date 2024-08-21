504fccd92ea08d63b31b0dec353a96ec6c756545
winiyaz <winiyaz@proton.me>
2024-08-21 09:20:44 -0700

    😊 w210 - Add Widget Elements

--- w210/main.py
+++ w210/main.py
@@ -12,9 +12,20 @@ window.minsize(width=600, height=300)
 header_label = Label(text='Lesson 210', font=('Arial', 24, "bold"))
 header_label.pack()
 
-#--- D27
+
+# --- button
+def bu1_click():
+	print("Got Click")
+	new_text = input1.get()  # Get input from entry
+	header_label.config(text=new_text)
 
 
+bu1 = Button(text="CLICKME", command=bu1_click)
+bu1.pack()
+
+# -- Text Entry
+input1 = Entry(width=50, font=('Arial', 20), justify="center")
+input1.pack(pady=10)
 
 # -- default loop
 window.mainloop()
